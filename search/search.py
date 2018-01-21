830e8a7bd7c300edb90cb31869de9d1ceb6d4270
fnzue <35587269+fnzue@users.noreply.github.com>
2018-01-20 22:53:27 -0500

    Questions 1 à 4 que j'ai fait avec une autre méthode plus courte
    Peux-tu tester ce fichier de ton côté stp ? De mon côté ça marche

--- search/search.py
+++ search/search.py
@@ -23,131 +23,163 @@ class SearchProblem:
     """
     This class outlines the structure of a search problem, but doesn't implement
     any of the methods (in object-oriented terminology: an abstract class).
-
     You do not need to change anything in this class, ever.
     """
 
     def getStartState(self):
         """
-        Returns the start state for the search problem.
+        Returns the start state for the search problem
         """
         util.raiseNotDefined()
 
     def isGoalState(self, state):
         """
           state: Search state
-
-        Returns True if and only if the state is a valid goal state.
+        Returns True if and only if the state is a valid goal state
         """
         util.raiseNotDefined()
 
     def getSuccessors(self, state):
         """
           state: Search state
-
-        For a given state, this should return a list of triples, (successor,
-        action, stepCost), where 'successor' is a successor to the current
-        state, 'action' is the action required to get there, and 'stepCost' is
-        the incremental cost of expanding to that successor.
+        For a given state, this should return a list of triples,
+        (successor, action, stepCost), where 'successor' is a
+        successor to the current state, 'action' is the action
+        required to get there, and 'stepCost' is the incremental
+        cost of expanding to that successor
         """
         util.raiseNotDefined()
 
     def getCostOfActions(self, actions):
         """
          actions: A list of actions to take
-
-        This method returns the total cost of a particular sequence of actions.
-        The sequence must be composed of legal moves.
+        This method returns the total cost of a particular sequence of actions.  The sequence must
+        be composed of legal moves
         """
         util.raiseNotDefined()
 
 
 def tinyMazeSearch(problem):
     """
-    Returns a sequence of moves that solves tinyMaze.  For any other maze, the
-    sequence of moves will be incorrect, so only use this for tinyMaze.
+    Returns a sequence of moves that solves tinyMaze.  For any other
+    maze, the sequence of moves will be incorrect, so only use this for tinyMaze
     """
     from game import Directions
     s = Directions.SOUTH
     w = Directions.WEST
-    return  [s, s, w, s, w, w, s, w]
+    return  [s,s,w,s,w,w,s,w]
 
 def depthFirstSearch(problem):
+
     """
-    Search the deepest nodes in the search tree first.
-
-    Your search algorithm needs to return a list of actions that reaches the
-    goal. Make sure to implement a graph search algorithm.
-
+    Search the deepest nodes in the search tree first
+    Your search algorithm needs to return a list of actions that reaches
+    the goal.  Make sure to implement a graph search algorithm
     To get started, you might want to try some of these simple commands to
     understand the search problem that is being passed in:
-
     print "Start:", problem.getStartState()
     print "Is the start a goal?", problem.isGoalState(problem.getStartState())
     print "Start's successors:", problem.getSuccessors(problem.getStartState())
     """
     "*** YOUR CODE HERE ***"
-
-    s = util.Stack()
-    markedState = []
-    if (problem.isGoalState(problem.getStartState())):
-        markedState.append(problem.getStartState())
-    else:
-        s.push(problem.getStartState())
-        while s.isEmpty() == False:
-            state = s.pop()
-            markedState.append(state)
-            if (problem.isGoalState(state)):
-                break
-            else:
-                successors = problem.getSuccessors(state)
-                """for successor in successors:
-                    if (successor[0] not in markedState):
-                        s.push(state)
-                        s.push(successor[0])"""
-                for i in range(0, len(successors)):
-                    successor = successors[len(successors)-1-i]
-                    if (successor[0] not in markedState):
-                        s.push(state)
-                        s.push(successor[0])
-
-
-
-    actions = []
-    for i in range(0,len(markedState)-1):
-        sm = markedState[i]
-        sn = markedState[i+1]
-        successors = problem.getSuccessors(sm)
-        for successor in successors:
-            if sn == successor[0]:
-                actions.append(successor[1])
-                break
-    return actions
-
-    util.raiseNotDefined()
+    curState = problem.getStartState()
+    successors = util.Stack()
+    successors.push((curState, [], 0))
+    markedSate = set()
+    while successors.isEmpty()!= True:
+        successor = successors.pop()
+        state = successor[0]
+        direction = successor[1]
+        cost = successor[2]
+        if problem.isGoalState(state):
+            return direction
+        else :
+            if state not in markedSate:
+                markedSate.add(state)
+                for coordinate, action, cos in problem.getSuccessors(state):
+                    if coordinate not in markedSate:
+                        successors.push((coordinate, direction + [action], cos))
+    return direction
 
 def breadthFirstSearch(problem):
-    """Search the shallowest nodes in the search tree first."""
+    """
+    Search the shallowest nodes in the search tree first.
+    """
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+
+    curState = problem.getStartState()
+    successors = util.Queue()
+    successors.push((curState, [], 0))
+    markedSate = set()
+    while successors.isEmpty()!= True:
+        successor = successors.pop()
+        state = successor[0]
+        direction = successor[1]
+        cost = successor[2]
+        if problem.isGoalState(state):
+            return direction
+        else :
+            if state not in markedSate:
+                markedSate.add(state)
+                for coordinate, action, cos in problem.getSuccessors(state):
+                    if coordinate not in markedSate:
+                        successors.push((coordinate, direction + [action], cos))
+    return direction
 
 def uniformCostSearch(problem):
-    """Search the node of least total cost first."""
+    "Search the node of least total cost first. "
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    curState = problem.getStartState()
+    successors = util.PriorityQueue()
+    successors.push((curState, [], 0), 0)
+    markedSate = set()
+    while successors.isEmpty()!= True:
+        successor = successors.pop()
+        state = successor[0]
+        direction = successor[1]
+        cost = successor[2]
+        if problem.isGoalState(state):
+            return direction
+        else :
+            if state not in markedSate:
+                markedSate.add(state)
+                for coordinate, action, cos in problem.getSuccessors(state):
+                    if coordinate not in markedSate:
+                        successors.push((coordinate, direction + [action], cost+cos), cos+cost)
+    return direction
 
-def nullHeuristic(state, problem=None):
+
+def nullHeuristic(state, problem = None):
     """
     A heuristic function estimates the cost from the current state to the nearest
     goal in the provided SearchProblem.  This heuristic is trivial.
     """
     return 0
 
-def aStarSearch(problem, heuristic=nullHeuristic):
-    """Search the node that has the lowest combined cost and heuristic first."""
+def aStarSearch(problem, heuristic = nullHeuristic):
+    "Search the node that has the lowest combined cost and heuristic first."
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
-
+    curState = problem.getStartState()
+    successors = util.PriorityQueue()
+    successors.push((curState, [], 0), 0)
+    markedSate = set()
+    while successors.isEmpty()!= True:
+        successor = successors.pop()
+        state = successor[0]
+        direction = successor[1]
+        cost = successor[2]
+        if problem.isGoalState(state):
+            return direction
+        else :
+            if state not in markedSate:
+                markedSate.add(state)
+                for coordinate,action,cos in problem.getSuccessors(state):
+                    if coordinate not in markedSate:
+                        backCost = cost + cos
+                        nextCost = heuristic(coordinate, problem)
+                        function = backCost + nextCost
+                        successors.push((coordinate, direction + [action], backCost), function)
+    return direction
 
 # Abbreviations
 bfs = breadthFirstSearch
